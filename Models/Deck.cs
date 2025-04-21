d8e87cf0d1fe8daf17a0482edae83e3fe6c66f11
Wraami <83980439+Wraami@users.noreply.github.com>
2025-04-21 19:52:44 +0100

    feat: provide basic deck class before the stakeholders get to me

--- Models/Deck.cs
+++ Models/Deck.cs
@@ -1,7 +1,43 @@
-﻿namespace BlackJackQueen.Models
+﻿using BlackJackQueen.Models.Enums;
+
+namespace BlackJackQueen.Models
 {
     //TODO: Add logic for ranks and suits within a deck.
-    internal class Deck
+    public class Deck
     {
+        public int CardsLeft { get; set; }
+        public List<CardModel>? Cards { get; set; }
+
+        public Deck()
+        {
+            Cards = new List<CardModel>();
+            GenerateDeck();
+        }
+
+        private void GenerateDeck()
+        {
+            foreach (Suit suit in Enum.GetValues<Suit>())
+            {
+                foreach (Rank rank in Enum.GetValues<Rank>())
+                {
+                    Cards.Add(new CardModel(suit, rank));
+                }
+            }
+        }
+
+        //TODO: human hand shuffle logic? how would this be accounted for? migrate away from use of random :)
+        private void Shuffle()
+        {
+            var rng = new Random();
+
+            for (int i = 0; i < Cards.Count; i++)
+            {
+                int j = rng.Next(Cards.Count);
+                var tempCard = Cards[i];
+                Cards[i] = Cards[j];
+                Cards[j] = tempCard;
+            }
+
+        }
     }
 }
