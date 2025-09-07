13dae470d64fdd41f56400802d283097f1023c21
Wraami <83980439+Wraami@users.noreply.github.com>
2025-09-07 19:35:56 +0100

    test: add failing test for splitting hand logic

---
+++ tests/BlackJackQueen.Tests/HandTests.cs
@@ -0,0 +1,23 @@
+﻿using BlackJackQueen.Models;
+using BlackJackQueen.Services;
+
+namespace BlackJackQueen.Tests;
+
+public class HandTests
+{
+    [Fact]
+    public void Split_ShouldCreateSeparateHands_WhenOneHandHasPair()
+    {
+        var hand = new Hand();
+
+        hand.AddCard(new CardModel(Models.Enums.Suit.Diamonds, Models.Enums.Rank.Six));
+        hand.AddCard(new CardModel(Models.Enums.Suit.Spades, Models.Enums.Rank.Six));
+
+        var gameService = new GameService();
+
+        gameService.SplitHand(hand);
+
+        Assert.Single(hand.cards);
+
+    }
+}
