807a61f41d4c31152d13d1ad758e0347955d869d
Wraami <83980439+Wraami@users.noreply.github.com>
2025-09-28 12:09:05 +0100

    feat: implement basic player class with optional wallet

---
+++ src/BlackJackQueen/Models/Player/Player.cs
@@ -0,0 +1,17 @@
+﻿using BlackJackQueen.Interfaces;
+
+namespace BlackJackQueen.Models.Player
+{
+    public class Player
+    {
+        public IWallet? Wallet { get; }
+        public string PlayerName { get; private set; }
+        public decimal Balance => Wallet.Balance;
+
+        public Player(string playerName, IWallet? wallet = null)
+        {
+            Wallet = wallet;
+            PlayerName = playerName;
+        }
+    }
+}
