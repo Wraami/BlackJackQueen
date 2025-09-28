c5d743b3949ac0b5c578baeab11a2cb645a151b3
Wraami <83980439+Wraami@users.noreply.github.com>
2025-09-28 11:58:47 +0100

    test: initial failing test for player class implementation

---
+++ tests/BlackJackQueen.Tests/PlayerTests.cs
@@ -0,0 +1,18 @@
+﻿using BlackJackQueen.Models.Player;
+
+namespace BlackJackQueen.Tests
+{
+    public class PlayerTests
+    {
+        [Theory]
+        [InlineData("Lizzy")]
+        [InlineData("Kitty")]
+        [InlineData("Regina")]
+        public void Player_ShouldBeAllowedToSetName_WhenConstructed(string playerName)
+        {
+            var player = new Player(playerName);
+            Assert.Equal(player.PlayerName, playerName);
+        }
+
+    }
+}
